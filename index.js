0a89accd074a5b01c4bda4b07a35913802ba3028
Aadarsh <aadarshkumar8871860855@gmail.com>
2023-06-25 11:00:02 +0530

    option cahining added in index.js

--- index.js
+++ index.js
@@ -19,34 +19,34 @@ Connection()
 let users = [];
 
 const addUser = (userData, socketId) => {
-    !users.some(user => user.sub === userData.sub) && users.push({ ...userData, socketId });
+    !users?.some(user => user?.sub === userData?.sub) && users?.push({ ...userData, socketId });
 }
 
 const removeUser = (socketId) => {
-    users = users.filter(user => user.socketId !== socketId);
+    users = users?.filter(user => user?.socketId !== socketId);
 }
 
 const getUser = (userId) => {
-    return users.find(user => user.sub === userId);
+    return users?.find(user => user?.sub === userId);
 }
 
 io.on('connection', (socket) => {
 
     //connect
-    socket.on("addUser", userData => {
-        addUser(userData, socket.id);
+    socket?.on("addUser", userData => {
+        addUser(userData, socket?.id);
         io.emit("getUsers", users);
     })
 
     //send message
-    socket.on('sendMessage', (data) => {
-        const user = getUser(data.receiverId);
-        io.to(user.socketId).emit('getMessage', data)
+    socket?.on('sendMessage', (data) => {
+        const user = getUser(data?.receiverId);
+        io.to(user?.socketId).emit('getMessage', data)
     })
 
     //disconnect
-    socket.on('disconnect', () => {
-        removeUser(socket.id);
+    socket?.on('disconnect', () => {
+        removeUser(socket?.id);
         io.emit('getUsers', users);
     })
 })
